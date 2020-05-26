e00393e6e055177267a8244b1fc3003297f705e8
zaxwebs <zifidigital@gmail.com>
2020-05-26 23:27:39 +0530

    Added register view

--- app/Controllers/Authentication.php
+++ app/Controllers/Authentication.php
@@ -10,6 +10,8 @@ class Authentication extends BaseController
 
 	public function register()
 	{
+		helper('form');
+		echo view('Authentication/register');
 	}
 
 	public function logout()
--- app/Views/Authentication/register.php
+++ app/Views/Authentication/register.php
@@ -2,12 +2,12 @@
 
 <?= $this->section('content') ?>
     <div class="container">
-        <h3>Login</h3>
+        <h3>Register</h3>
 				<div class="row">
 					<div class="col-md-5">
 						<?= form_open('/login') ?>
 						<input type="text" class="form-control mb-2" placeholder="Enter your email">
-						<input type="password" class="form-control mb-2" placeholder="Enter your password">
+						<input type="password" class="form-control mb-2" placeholder="Set a password">
 						<input type="submit" class="btn btn-primary" value="Login">
 						</form>
 					</div>
