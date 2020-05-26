31a0ec8e12d34144c4747bd22db86d98960fd688
zaxwebs <zifidigital@gmail.com>
2020-05-27 04:00:30 +0530

    Chnaged register view

--- app/Views/Authentication/register.php
+++ app/Views/Authentication/register.php
@@ -2,13 +2,13 @@
 
 <?= $this->section('content') ?>
     <div class="container">
-        <h3>Register</h3>
+        <h3>Become a Member</h3>
 				<div class="row">
 					<div class="col-md-5">
 						<?= form_open('/login') ?>
 						<input type="text" class="form-control mb-2" placeholder="Enter your email">
 						<input type="password" class="form-control mb-2" placeholder="Set a password">
-						<input type="submit" class="btn btn-primary" value="Login">
+						<input type="submit" class="btn btn-primary" value="Signup">
 						</form>
 					</div>
 				</div>
